e190c96d2a53ddece00ac91728248d405ab69939
EfimowEgor <egor.efimowA@yandex.ru>
2022-06-12 19:35:15 +0500

    Удаление старых данных со страницы анализа

--- main.py
+++ main.py
@@ -34,29 +34,6 @@ minus_profit = "#FAA2A2"  # hex red color
 zero_porfit = "#F9F9FB"  # hex white color
 plus_profit = "#47F19F "  # hex green color
 
-# # APP STATES
-#
-# STATE_LOGIN = 0
-# STATE_MAIN = 1
-#
-#
-# class CLoginState:
-#     def __init__(self):
-#         super(CLoginState, self).__init__()
-#         self.login = LogWindow()
-#
-#     def start(self):
-#         self.login.show()
-#
-#
-# class CMainState:
-#     def __init__(self):
-#         super(CMainState, self).__init__()
-#         self.work_win = MainWindow()
-#
-#     def start(self):
-#         self.work_win.show()
-#
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -78,9 +55,6 @@ class MainWindow(QMainWindow):
         self.ui.legeng_analytic_pie_chart.horizontalScrollBar().setStyleSheet('QScrollBar {height:0px;}')
         self.main_pie_chart = QtWebEngineWidgets.QWebEngineView(self.ui.widget)
         self.analytic_pie_chart = QtWebEngineWidgets.QWebEngineView(self.ui.analytics_graps_widget)
-
-        # self.ui.to_login_2.clicked.connect(self.btn_to_login)
-        # self.ui.to_login_2.clicked.connect(self.close)
 
         def moveWindow(event):
             if UIFuncs.returnStatus(self) == 1:
@@ -356,13 +330,9 @@ class MainWindow(QMainWindow):
             all_instruments_df['cost'] = pd.Series()
             print("Here")
             '''
-            
-            
             УДАЛЕНИЕ ДЕЛАТЬ ТУТ ! 
-            
-            
-            
             '''
+            self.clear_all_instruments_on_analytic_page()
         else:
             all_instruments_df = pd.DataFrame(list_pos)
         all_instruments_df = all_instruments_df.sort_values('cost', ascending=False)
@@ -692,8 +662,6 @@ class MainWindow(QMainWindow):
         # Все инструменты.
         self.fill_all_instruments_on_analytic_page()
 
-
-
     def fill_all_instruments_on_analytic_page(self):
         global full_df
         full_df = full_df.sort_values('cost', ascending=False)
@@ -729,89 +697,15 @@ class MainWindow(QMainWindow):
             else:
                 pass
 
+    def clear_all_instruments_on_analytic_page(self):
+        for i in reversed(range(self.all_instruments_analytic_page_list.count())):
+            self.all_instruments_analytic_page_list.takeAt(i).widget().setParent(None)
+
     def fill_portfolio(self, token, account):
         sender = self.sender()
         with Client(token) as client:
             portfolio = client.operations.get_portfolio(account_id=account.id)
             self.fill_main_page(portfolio, token, client)
-
-
-#
-# class LogWindow(QMainWindow):
-#     def __init__(self):
-#         QFontDatabase.addApplicationFont('Ubuntu-Regular.ttf')
-#         QMainWindow.__init__(self)
-#         self.dragPos = None
-#         self.ui = Ui_MainWindow()
-#         self.ui.setupUi(self)
-#         self.ui.btn_show_password_2.clicked.connect(self.toggleVisibility)
-#
-#         # Move Window
-#         def moveWindow(event):
-#             # If left click - move window
-#             if event.buttons() == Qt.LeftButton:
-#                 self.move(self.pos() + event.globalPos() - self.dragPos)
-#                 self.dragPos = event.globalPos()
-#                 event.accept()
-#
-#         # Set title bar
-#         self.ui.title_bar_2.mouseMoveEvent = moveWindow
-#
-#         UIFunctions.uiDefinitions(self)
-#
-#         # Смена окон в MainWindow Login
-#
-#         # Login Window
-#         self.ui.btn_signin_4.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_reg))
-#
-#         # Registration Window
-#         self.ui.btn_signin_6.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_login))
-#
-#         # Переход к окну программы с метриками
-#         self.ui.btn_signin_3.clicked.connect(self.btn_to_main_clicked)
-#         self.ui.btn_signin_5.clicked.connect(self.btn_to_main_clicked)
-#
-#         # Закрытие окна
-#         self.ui.btn_signin_3.clicked.connect(self.close)
-#         self.ui.btn_signin_5.clicked.connect(self.close)
-#
-#     def mousePressEvent(self, event):
-#         self.dragPos = event.globalPos()
-#
-#     # Изменение видимости пароля
-#     def toggleVisibility(self):
-#         if self.ui.edit_password_2.echoMode() == QLineEdit.Normal:
-#             self.ui.edit_password_2.setEchoMode(QLineEdit.Password)
-#             new_icon = QPixmap('closedEyeChanged.png')
-#             self.ui.btn_show_password_2.setIcon(QIcon(new_icon))
-#         else:
-#             self.ui.edit_password_2.setEchoMode(QLineEdit.Normal)
-#             new_icon = QPixmap('openedEye.png')
-#             self.ui.btn_show_password_2.setIcon(QIcon(new_icon))
-#
-#     def btn_to_main_clicked(self):
-#         app.procNextState(STATE_MAIN)
-#
-#
-# class CMainApp:
-#     def __init__(self):
-#         self.app = QApplication(sys.argv)
-#         self.s = None
-#
-#     def __CreateState(self, state):
-#         if state == STATE_LOGIN:
-#             self.s = CLoginState()
-#         if state == STATE_MAIN:
-#             self.s = CMainState()
-#         return self.s
-#
-#     def procNextState(self, state):
-#         s = self.__CreateState(state)
-#         s.start()
-#
-#     def run(self):
-#         self.procNextState(STATE_LOGIN)
-#         sys.exit(self.app.exec_())
 
 
 if __name__ == "__main__":
